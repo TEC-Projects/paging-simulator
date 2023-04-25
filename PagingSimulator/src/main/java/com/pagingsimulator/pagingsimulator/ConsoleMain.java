b72a3abfd503551ca90713e10161a7fd44046067
Josh <joshgc.19@estudiantec.cr>
2023-04-25 14:55:37 -0600

    jaja

--- PagingSimulator/src/main/java/com/pagingsimulator/pagingsimulator/ConsoleMain.java
+++ PagingSimulator/src/main/java/com/pagingsimulator/pagingsimulator/ConsoleMain.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class ConsoleMain {
     public static void main(String[] args) throws IOException {
         OperationsFileManager operationsFileManager = new OperationsFileManager();
-        operationsFileManager.retrieveOperationsFromFile(new File("C:\\Users\\joshg\\Documents\\Projects\\paging-simulator\\PagingSimulator\\src\\main\\java\\com\\pagingsimulator\\pagingsimulator\\op.txt"));
+//        System.out.println(operationsFileManager.retrieveOperationsFromFile(new File("C:\\Users\\joshg\\Documents\\Projects\\paging-simulator\\PagingSimulator\\src\\main\\java\\com\\pagingsimulator\\pagingsimulator\\op.txt")));
         System.out.println(operationsFileManager.convertOperationsToFileString(operationsFileManager.generateOperations(123, 100, 20)));
     }
 }
