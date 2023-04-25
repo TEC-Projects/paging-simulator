1939811860ca6a5d48efb593cd8f0b1c44dc4713
Josh <joshgc.19@estudiantec.cr>
2023-04-25 12:28:41 -0600

    intial structure

--- PagingSimulator/src/main/java/com/pagingsimulator/pagingsimulator/Model/Machine.java
+++ PagingSimulator/src/main/java/com/pagingsimulator/pagingsimulator/Model/Machine.java
@@ -1,4 +1,70 @@
 package com.pagingsimulator.pagingsimulator.Model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Machine {
+    private int pageCount;
+    private int ptrCount;
+    private HashMap<Integer, Allocation> memoryMap;
+    private Page[] realMemory;
+    private ArrayList<Page> virtualMemory;
+    private HashMap<Integer, Page> pages;
+    private ArrayList<Process> processes;
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+
+    public int getPtrCount() {
+        return ptrCount;
+    }
+
+    public void setPtrCount(int ptrCount) {
+        this.ptrCount = ptrCount;
+    }
+
+    public HashMap<Integer, Allocation> getMemoryMap() {
+        return memoryMap;
+    }
+
+    public void setMemoryMap(HashMap<Integer, Allocation> memoryMap) {
+        this.memoryMap = memoryMap;
+    }
+
+    public Page[] getRealMemory() {
+        return realMemory;
+    }
+
+    public void setRealMemory(Page[] realMemory) {
+        this.realMemory = realMemory;
+    }
+
+    public ArrayList<Page> getVirtualMemory() {
+        return virtualMemory;
+    }
+
+    public void setVirtualMemory(ArrayList<Page> virtualMemory) {
+        this.virtualMemory = virtualMemory;
+    }
+
+    public HashMap<Integer, Page> getPages() {
+        return pages;
+    }
+
+    public void setPages(HashMap<Integer, Page> pages) {
+        this.pages = pages;
+    }
+
+    public ArrayList<Process> getProcesses() {
+        return processes;
+    }
+
+    public void setProcesses(ArrayList<Process> processes) {
+        this.processes = processes;
+    }
 }
